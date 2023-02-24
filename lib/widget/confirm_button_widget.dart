99c9ea664c0d097e54b0176eb75eb06321c2ebd1
Daffa Rafif Agustian <agustian2504@gmail.com>
2023-02-24 07:31:40 +0700

    feat:perbaikan button dan text dialog

--- lib/widget/confirm_button_widget.dart
+++ lib/widget/confirm_button_widget.dart
@@ -18,7 +18,7 @@ class ActionButtonConfirmWidget extends StatelessWidget {
             builder: (context) {
               return AlertDialog(
                 title: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 0),
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
                   child: Text(
                     "Please Note",
                     style: TextStyle(color: Color(0xff000000)),
@@ -36,31 +36,40 @@ class ActionButtonConfirmWidget extends StatelessWidget {
                     children: [
                       ListTile(
                         title: Container(
-                          child: Row(
+                          child: Wrap(
                             children: [
                               Container(
-                                child: Text('DELIVERY TO MAINLAND ',
-                                style: TextStyle(color: Colors.grey),),
+                                child: Text(
+                                  'DELIVERY TO MAINLAND ',
+                                  style: TextStyle(color: Colors.grey),
+                                ),
                               ),
                               SizedBox(
                                 height: 30,
+                              ),
+                              Container(
+                                child: Text("N1000 - N2000"),
                               ),
                             ],
                           ),
                         ),
                       ),
-                      Divider(
-                          color: Colors.black26, indent: 15, endIndent: 20),
+                      Divider(color: Colors.black26, indent: 15, endIndent: 20),
                       ListTile(
                         title: Container(
-                          child: Row(
+                          child: Wrap(
                             children: [
                               Container(
-                                child: Text('DELIVERY TO ISLAND ',
-                                  style: TextStyle(color: Colors.grey),),
+                                child: Text(
+                                  'DELIVERY TO ISLAND ',
+                                  style: TextStyle(color: Colors.grey),
+                                ),
                               ),
                               SizedBox(
                                 height: 30,
+                              ),
+                              Container(
+                                child: Text("N2000 - N3000"),
                               ),
                             ],
                           ),
@@ -70,30 +79,42 @@ class ActionButtonConfirmWidget extends StatelessWidget {
                   ),
                 ),
                 actions: [
-                  ElevatedButton(
-                    onPressed: () => print("Confirm cancel press"),
-                    child: Text(
-                      "Cancel",style: TextStyle(color: Colors.grey),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xffffffff),
-                      shape:
-                      RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0),
+                  Container(
+                    child: TextButton(
+                      onPressed: () {
+                        print("Confrim cancel press");
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        "Cancel",
+                        style: TextStyle(color: Colors.grey),
                       ),
-                      minimumSize: Size(100.0, 60.0),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Color(0xffffffff),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        minimumSize: Size(100.0, 60.0),
+                      ),
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: () => print("Confrim proced pres"),
-                    child: Text(
-                      "Proced",
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.deepOrange,
-                      shape:
-                      RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0),
+                  SizedBox(
+                    width: 159,
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print("Confrim proced pres");
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        "Proced",
                       ),
-                      minimumSize: Size(100.0, 60.0),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepOrange,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
                     ),
                   ),
                 ],
